a2bc90604897e24dc30405481a3d5617a4891aff
ZaheenFatima <66130656+ZaheenFatima@users.noreply.github.com>
2020-07-14 10:20:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,10 +4,11 @@ var bullet,speed,weight;
 function setup() {
   createCanvas(1600,400);
 
+  thickness = random(22,83);
+  
  bullet = createSprite(50,200,50,50);
  wall = createSprite(1200,200,thickness,height/2);
 
- thickness = random(22,83);
  speed = random(223,321);
  weight = random(30,52);
 }
